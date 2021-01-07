9435ca705937ad5765c7bf891b3660a70ed74e7e
qiankun <1975210542@qq.com>
2021-01-07 15:56:35 +0800

    add router

---
+++ internal/routers/router.go
@@ -0,0 +1,26 @@
+package routers
+
+import "github.com/gin-gonic/gin"
+
+func NewRouter() *gin.Engine {
+	r := gin.New()
+	r.Use(gin.Logger())
+	r.Use(gin.Recovery())
+
+	apiv1 := r.Group("/api/v1")
+	{
+		apiv1.POST("/tags")
+		apiv1.DELETE("/tags/:id")
+		apiv1.PUT("/tags/:id")
+		apiv1.PATCH("/tag/:id/state")
+		apiv1.GET("/tags")
+
+		apiv1.POST("/articles")
+		apiv1.DELETE("/articles/:id")
+		apiv1.PUT("/articles/:id")
+		apiv1.PATCH("/articles/:id/state")
+		apiv1.GET("/articles")
+		apiv1.GET("/articles/:id")
+	}
+	return r
+}
